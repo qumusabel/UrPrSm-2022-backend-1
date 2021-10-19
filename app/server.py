d28524e3c8b68148cd0515702463df9225bd28e3
qumusabel <fim2774@gmail.com>
2021-10-20 00:02:11 +0500

    `server.py` cleanup

--- app/server.py
+++ app/server.py
@@ -2,18 +2,20 @@
 
 import json
 
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from fastapi.requests import Request
 from fastapi.responses import JSONResponse
+from fastapi.exceptions import HTTPException
 
 from . import db
 from .utils import generate_slug, is_valid_url
 
 
-app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
+def _error(code: int, msg: str) -> HTTPException:
+    return HTTPException(status_code=code, detail={"status": "Error", "msg": msg})
 
-def _error(code: int, msg: str) -> JSONResponse:
-    return JSONResponse({"status": "Error", "msg": msg}, code)
+
+app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
 
 
 @app.on_event('startup')
@@ -26,21 +28,27 @@ async def shorten(request: Request):
     try:
         req_json = await request.json()
     except json.JSONDecodeError:
-        return _error(400, "invalid JSON")
+        raise _error(status.HTTP_400_BAD_REQUEST, "invalid JSON")
 
     if 'urlToShorten' not in req_json:
-        return _error(400, "missing 'urlToShorten'")
+        raise _error(status.HTTP_400_BAD_REQUEST, "missing 'urlToShorten'")
 
     url = req_json.get('urlToShorten')
 
     if not is_valid_url(url):
-        return _error(400, "invalid URL")
+        raise _error(status.HTTP_400_BAD_REQUEST, "invalid URL")
 
     slug = generate_slug()
 
     await db.add_url(slug, url)
 
-    return JSONResponse({'url': str(request.base_url) + slug}, 201)
+    return JSONResponse(
+        {
+            'status': "Created", 
+            "shortenedUrl": str(request.base_url) + slug
+        },
+        status.HTTP_201_CREATED
+    )
 
 
 @app.get('/{slug}')
@@ -48,15 +56,17 @@ async def get_url(slug: str):
     url = await db.get_url(slug)
 
     if url is None:
-        return _error(404, "not found")
+        raise _error(status.HTTP_404_NOT_FOUND, "not found")
 
     await db.inc_hits(slug)
 
     return JSONResponse(
-            {"redirectTo": url}, 
-            status_code=301, 
-            headers={"Location": url}
-        )
+        {
+            "redirectTo": url
+        }, 
+        status.HTTP_301_MOVED_PERMANENTLY,
+        headers={"Location": url}
+    )
 
 
 @app.get('/{slug}/views')
@@ -64,7 +74,7 @@ async def get_slug_views(slug: str):
     views = await db.get_hits(slug)
 
     if views is None:
-        return _error(404, "not found")
+        raise _error(status.HTTP_404_NOT_FOUND, "not found")
 
-    return JSONResponse({"viewCount": views}, 200)
+    return JSONResponse({"viewCount": views}, status.HTTP_200_OK)
 
