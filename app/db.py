9685bcf1e0f849b87b65a21a930e8b64e453e683
qumusabel <fim2774@gmail.com>
2021-10-20 00:13:51 +0500

    `db.py` use SQL arithmetic

--- app/db.py
+++ app/db.py
@@ -1,14 +1,15 @@
 import os
-from typing import Tuple, Union
+from typing import Union
 
 import aiosqlite
 
-_db_file = os.getenv('SQLITE_FILE', ':memory:')
+
+_db_file = os.getenv('SQLITE_DB', '/tmp/db.sqlite')
 
 _schema = "CREATE TABLE IF NOT EXISTS urls (slug TEXT, url TEXT, hits INTEGER)"
 _get_url_sql  = "SELECT * FROM urls WHERE slug = (?)"
 _add_url_sql  = "INSERT INTO urls VALUES (?, ?, 0)"
-_inc_hits_sql = "UPDATE urls SET hits = (?) WHERE slug = (?)" 
+_inc_hits_sql = "UPDATE urls SET hits = hits + 1 WHERE slug = (?)"
 
 
 async def init_db():
@@ -43,12 +44,6 @@ async def get_hits(slug: str) -> Union[int, None]:
 
 async def inc_hits(slug: str):
     async with aiosqlite.connect(_db_file) as db:
-        row = await _get_row(slug)
-        
-        if row is None:
-            return
-
-        hits = row['hits']
-
-        await db.execute(_inc_hits_sql, (hits + 1, slug))
+        await db.execute(_inc_hits_sql, (slug, ))
         await db.commit()
+
